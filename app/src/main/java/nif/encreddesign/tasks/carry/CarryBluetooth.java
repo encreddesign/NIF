c8fb45991598a37c1505dd12a5383b1270e6aa4c
encreddesign <encredgaming@gmail.com>
2017-02-15 17:42:24 +0000

    Added carry tasks

---
+++ app/src/main/java/nif/encreddesign/tasks/carry/CarryBluetooth.java
@@ -0,0 +1,7 @@
+package nif.encreddesign.tasks.carry;
+
+/**
+ * Created by Joshua on 15/02/17.
+ */
+public class CarryBluetooth {
+}
---
+++ app/src/main/java/nif/encreddesign/tasks/carry/ICarry.java
@@ -0,0 +1,13 @@
+package nif.encreddesign.tasks.carry;
+
+/**
+ * Created by Joshua on 15/02/17.
+ */
+public interface ICarry {
+
+    /*
+    * @method init
+    * */
+    public void init ();
+
+}
